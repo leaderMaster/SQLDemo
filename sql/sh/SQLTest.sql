5e3aa84cd0f8802dabc016ff156ad9f54a70aa66
BraveStarr <javaseee@126.com>
2024-08-26 15:21:57 +0800

    SQLDemo sql sh add SQLTest.sql 生成百万级别的数据文件, 用户: sh, 表: sales, 数据量: 92 万

--- sql/sh/SQLTest.sql
+++ sql/sh/SQLTest.sql
@@ -3,3 +3,13 @@ ALTER USER SH ACCOUNT UNLOCK;
 
 -- 修改用户密码
 ALTER USER SH IDENTIFIED BY 123456;
+
+-- 生成百万级别的数据文件, 用户: sh, 表: sales, 数据量: 92 万
+SELECT count(*)
+FROM (SELECT p.PROD_NAME                                  AS prod_name,
+             c.CUST_FIRST_NAME || ' ' || c.CUST_LAST_NAME AS cus_name
+      FROM PRODUCTS p,
+           SALES s,
+           CUSTOMERS c
+      WHERE p.PROD_ID = s.PROD_ID
+        AND s.CUST_ID = c.CUST_ID);
